eadd33146e8125fddc4ffc4fa0bb7e39c2ff7b1c
lmatani <lmatarrubia@gmail.com>
2024-07-14 18:17:36 +0200

    cambios workout_2

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,49 @@
+import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Home from './Home';
+import ItemDetailPage from './ItemDetailPage';
+
 const App = () => {
+  const [data, setData] =  useState(null);
+  const uriApi ='http://localhost:3000'
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(uriApi);
+      const resData = await response.json();
+      setData(resData);
+
+    } catch (error) {
+      console.error(error); 
+    }
+  }
+  
+  useEffect(() => {
+    fetchData()
+  }, []);
 
   return (
-  <></>      
+   
+      <Router>
+        <div>
+          <nav>
+            <Link to='/'>Inicio</Link>
+          </nav>
+          { data === null  
+            ? (<div>Cargando ...</div>)
+            : <Routes>
+              <Route path="/" element={<Home data={data} />} />
+              {
+                data.map(item => (
+                  <Route key={item._id} path={`/${item._id}`} element={<ItemDetailPage item={item}/>} />
+                ))
+              }
+            </Routes>
+          }
+         
+        </div>
+      </Router>
+     
   )
 };
 
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,21 @@
-const Home = () => {
+import { Link } from 'react-router-dom';
+
+
+const Home = ({data}) => {
   return (
     <>
-    
+      <div>
+        <h2>Lista de Datos</h2>
+        <ul>
+          {data.map(item => (
+            <li key={item._id}>
+              <Link to={`/${item._id}`}>{item.title}</Link>
+            </li>
+          )
+
+          )}
+        </ul>
+      </div>
     </>
   )
 };
